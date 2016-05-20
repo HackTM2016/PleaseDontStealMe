0a2a1c013532b12ab9b3dedc24fb2c22b5bdc54f
vldfr <vlad.fara@gmail.com>
2016-05-21 00:46:15 +0300

    untested brake functionality
    Just unimplemented brake functionality for the system and broke the code
    in readable bits

--- Smart_Bike.ino
+++ Smart_Bike.ino
@@ -1,6 +1,7 @@
 
 //constante
-int vitezometru = 2;
+const short vitezometru = 2;
+const short Stop = 3;
 
 
 const unsigned short wheel = 2080;
@@ -18,13 +19,18 @@ float viteza_anterioara = 0;  // pentru delta v
 float acceleratie = 0;        // a =  v/t 
 
 bool lastTypeClic = 1;
+bool CLICKLOOP = 0;
 
 void setup() {
   Serial.begin(9600);
   pinMode(vitezometru, INPUT_PULLUP);
+  pinMode(Stop, OUTPUT);
 }
 
 void loop() {
+
+  // Sectiunea de vitezometru
+  
   int clic = digitalRead(vitezometru);
   clicTime = millis() - lastClic;
   if(clic == 0 && lastTypeClic == 1)
@@ -33,6 +39,7 @@ void loop() {
     lastClic = millis();
     clicTime = 0;
     lastTypeClic = clic;
+    CLICKLOOP = 1; //IMPORTANT
   }
   else if(clic == 1 && lastTypeClic == 0)
   {
@@ -43,9 +50,33 @@ void loop() {
     viteza = 0;
   }
 
+  // END Sectiunea de vitezometru
+
+  //Sectiunea de detectie de frana
+  if(CLICKLOOP)
+  {
+    if(viteza != viteza_anterioara && (viteza_anterioara - viteza) > 6)
+    {
+      analogWrite(Stop, 255);
+    }
+    else if( (viteza - viteza_anterioara <= 6) && (viteza - viteza_anterioara > 0) )
+    {
+      analogWrite(Stop, 128);
+    }
+    else
+    {
+      analogWrite(Stop, 0);
+    }
+  
+    viteza_anterioara = viteza;
+  }
+  //END Sectiunea de detectie de frana
   
   Serial.print(viteza);
-    Serial.print(" , ");
-    Serial.println(clic);
+  Serial.print(" , ");
+  Serial.println(clic);
+  
+  CLICKLOOP = 0;
+  
   delay(1); // "daca da eroare maresti delayu;" Pava -2016
 }
